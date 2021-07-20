5d9c4ff6376aac0567a16ef9c4e6c35d643bdd2e
digogid <rodrigo.silva1302@gmail.com>
2021-07-20 10:50:21 -0300

    init pt2

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+import styles from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/new-meetup'>New Meetup</Link>
+          </li>
+          <li>
+            <Link to='/favorites'>Favorites</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,9 @@
+const NewMeetupPage = () => {
+    return (
+        <div>
+            New Meetup Page
+        </div>
+    );
+}
+
+export default NewMeetupPage;
